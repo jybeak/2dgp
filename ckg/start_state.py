2a9b12477ea0fae81306b41b2ae1bfd362e0203d
BEAK JONG YEON <whddus6712@naver.com>
2017-11-18 15:52:13 +0900

    title_state 추가

--- ckg/start_state.py
+++ ckg/start_state.py
@@ -1,4 +1,5 @@
 import game_framework
+import title_state
 from pico2d import *
 
 
@@ -28,7 +29,7 @@ def update():
 
     if (logo_time > 1.0):
         logo_time = 0
-        game_framework.quit()
+        game_framework.push_state(title_state)
     delay(0.01)
     logo_time += 0.01
 
